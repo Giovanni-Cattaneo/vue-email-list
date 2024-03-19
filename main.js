3367040ba02b14f2e665e752e097303ba270c977
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-19 13:59:11 +0100

    edit main js, remove var

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-            randomNumberUser: null,
-            randomNumberDesktop: null
+            
         }
     },
     methods: {
