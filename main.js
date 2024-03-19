9855ccca6b91213a137021e3850c3e16bc1cf1b5
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-19 14:11:52 +0100

    add ul of mailing list

--- main.js
+++ main.js
@@ -2,19 +2,26 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-            
+          mail: []
         }
     },
     methods: {
 
         callApi(){
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) =>{
-                console.log(response.data.response)
+
+            for (let i = 0; i < 10; i++) {
+                const element = this.mail[i];
+
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) =>{
+                this.mail.push(response.data.response)
             })
+                
+            }
+            console.log(this.mail);
+            
         }
     },
     mounted(){
-
         this.callApi()
     }
 }).mount("#app")
