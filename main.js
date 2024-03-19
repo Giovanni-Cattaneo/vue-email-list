1244cd0eed7b41819b93f476ff3d35ba047acddb
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-19 14:02:07 +0100

    add mounted and callApi function

--- main.js
+++ main.js
@@ -6,7 +6,15 @@ createApp({
         }
     },
     methods: {
+
+        callApi(){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) =>{
+                console.log(response.data.response)
+            })
+        }
     },
     mounted(){
+
+        this.callApi()
     }
 }).mount("#app")
